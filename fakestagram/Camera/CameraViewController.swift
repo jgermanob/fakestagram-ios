dad5853a75fa8cfa7c79d9a17cc4abb6ffdf1a5b
german <jgermanmx@gmail.com>
2019-05-25 13:29:06 -0500

    remove unnecessary functions and comments

--- fakestagram/Camera/CameraViewController.swift
+++ fakestagram/Camera/CameraViewController.swift
@@ -24,27 +24,6 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
-        /*let picker = YPImagePicker()
-        picker.didFinishPicking { [unowned picker] items, _ in
-            if let photo = items.singlePhoto {
-                print(photo.fromCamera) // Image source (camera or library)
-                print(photo.image) // Final image selected by the user
-                print(photo.originalImage) // original image selected by the user, unfiltered
-                print(photo.modifiedImage) // Transformed image, can be nil
-                print(photo.exifMeta) // Print exif meta data of original image.
-            }
-            self.dismiss(animated: true, completion: nil)
-            /*self.dismiss(animated: true, completion: {
-                print("self.dismiss()")
-                picker.dismiss(animated: true, completion: nil)
-            })*/
-            //picker.dismiss(animated: true, completion: nil)
-            self.tabBarController?.selectedIndex = 0
-            
-        }
-        present(picker, animated: true, completion: nil)*/
-        
     }
     
     func selectImage(){
@@ -52,11 +31,6 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate {
         let picker = YPImagePicker()
         picker.didFinishPicking { [unowned picker] items, _ in
             if let photo = items.singlePhoto {
-                print(photo.fromCamera) // Image source (camera or library)
-                print(photo.image) // Final image selected by the user
-                print(photo.originalImage) // original image selected by the user, unfiltered
-                print(photo.modifiedImage) // Transformed image, can be nil
-                print(photo.exifMeta) // Print exif meta data of original image.
                 self.postPreview.image = photo.image
             }
             
@@ -68,43 +42,12 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate {
                 self.isFirstTime = false
                 picker.dismiss(animated: true, completion: nil)
             }
-            //self.dismiss(animated: true, completion: nil)
-            /*self.dismiss(animated: true, completion: {
-             print("self.dismiss()")
-             picker.dismiss(animated: true, completion: nil)
-             })*/
-            //picker.dismiss(animated: true, completion: nil)
-            //self.tabBarController?.selectedIndex = 0
             
         }
         present(picker, animated: true, completion: nil)
     }
-
-    @IBAction func onTapSnap(_ sender: Any) {
-       /* guard let img = UIImage(named: "drake"),
-              let imgBase64 = img.encodedBase64() else { return }
-        let payload = CreatePostBase64(title: "Yaaas! - \(Date().currentTimestamp())",
-            imageData: imgBase64)
-        client.create(payload: payload) { post in
-            print(post)
-        }*/
-        
-        let pickerController = UIImagePickerController()
-        pickerController.allowsEditing = true
-        pickerController.delegate = self
-        
-        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        let galleryAction = UIAlertAction(title: "Gallery", style: .default) { (action) in
-            pickerController.sourceType = .photoLibrary
-            self.present(pickerController, animated: true, completion: nil)
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        actionSheet.addAction(galleryAction)
-        actionSheet.addAction(cancelAction)
-        self.present(actionSheet, animated: true, completion: nil)
-        
-    }
     
+    //Function to share a post once an image was selected from image picker
     @IBAction func sharePost(_ sender: UIButton) {
         print("Share")
         guard let image = postPreview.image as? UIImage, let imageBase64 = image.encodedBase64() else {return}
